0faf098df40b0c5ebbda8452e264638c2d516fdd
diegobarros <diegobarrosflf@gmail.com>
2020-11-17 20:29:43 -0300

    Testando REST

---
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.example.demo.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String test() {
+		return "REST Funcionando";
+	}
+
+}
